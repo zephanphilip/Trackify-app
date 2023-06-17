e38f5a3878a193acb09316a47366218d8e98b3f4
zephanphilip <zephanphilip03@gmail.com>
2023-06-17 10:59:30 +0530

    Updated code for bug fix

--- server/src/index.js
+++ server/src/index.js
@@ -1,30 +1,61 @@
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 const app = express();
 
-
 app.use(express.json());
 app.use(cors());
 
-// app.use("/auth", userRouter);
-// app.use("/add", incomeRouter);
-// app.use("/add", expenseRouter);
-
+// Routes
 app.use("/api/auth", require("./routes/users.js"));
 app.use("/api/add", require("./routes/income.js"));
 app.use("/api/add", require("./routes/expense.js"));
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(express.static(path.join(__dirname,'/build')));
+// Serve the React app for any other route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-app.get('/*', function(req, res) { 
-    res.sendFile(path.join(__dirname 
-    ,'/build/index.html')); }); 
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://123:123@cluster0.pvpoxvw.mongodb.net/Cluster0?retryWrites=true&w=majority")
+  .then(() => {
+    // Start the server
+    app.listen(3001, () => console.log('Server listening on port 3001'));
+  })
+  .catch(error => {
+    console.error('Failed to connect to MongoDB:', error);
+  });
 
-mongoose.connect("mongodb+srv://123:123@cluster0.pvpoxvw.mongodb.net/Cluster0?retryWrites=true&w=majority");
 
-app.listen(3001,()=> console.log('server listening on'));
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const path = require("path");
+// const app = express();
+
+
+// app.use(express.json());
+// app.use(cors());
+
+// // app.use("/auth", userRouter);
+// // app.use("/add", incomeRouter);
+// // app.use("/add", expenseRouter);
+
+// app.use("/api/auth", require("./routes/users.js"));
+// app.use("/api/add", require("./routes/income.js"));
+// app.use("/api/add", require("./routes/expense.js"));
+
+
+// app.use(express.static(path.join(__dirname,'/build')));
+
+// app.get('/*', function(req, res) { 
+//     res.sendFile(path.join(__dirname 
+//     ,'/build/index.html')); }); 
+
+// mongoose.connect("mongodb+srv://123:123@cluster0.pvpoxvw.mongodb.net/Cluster0?retryWrites=true&w=majority");
+
+// app.listen(3001,()=> console.log('server listening on'));
