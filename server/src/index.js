23955d0852ca6dd9be35243db0240f34302c9dc2
zephanphilip <zephanphilip03@gmail.com>
2023-06-17 11:40:01 +0530

    Updated code for bug fix

--- server/src/index.js
+++ server/src/index.js
@@ -17,11 +17,12 @@ app.use("/api/add", incomeRouter);
 app.use("/api/add", expenseRouter);
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/*', function(req, res) { 
-    res.sendFile(path.join(__dirname, 'build', 'index.html')); 
+    res.sendFile(path.join(__dirname, '../build', 'index.html')); 
 });
+
 
 
 
